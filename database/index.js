d6b48718bf617d2e51f35ca1d302d7d676a7575c
alviansmaulana <sandalproject7@gmail.com>
2022-02-06 15:02:24 +0700

    production testing

--- database/index.js
+++ database/index.js
@@ -11,8 +11,7 @@ const mongoose = require('mongoose');
 // local, changed to production -> heroku
 // mongoose.connect(process.env.MONGODB_URI);
 
-const MONGO_URI = "mongodb://localhost:27017/myapp"
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
